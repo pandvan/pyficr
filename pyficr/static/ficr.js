b9836837228a6e59fbc4068e135ada1c37593c5a
Andrea 'Pawz' Pavan <prog.pawz@gmail.com>
2016-05-21 18:22:42 +0200

    WebUI: clicking on event links it will copy the URL in the URL input box

--- pyficr/static/ficr.js
+++ pyficr/static/ficr.js
@@ -9,10 +9,18 @@ function fill_event_list() {
             //console.log("index: " + index);
             //console.log("link: " + this.link);
             //console.log("event: " + this.event);
-            $("#event-list").append("<li class='col-sm-6'><a href='"+this.link+"'>"+this.event+"</a></li>");
+            $("#event-list").append("<li class='col-sm-6'><a class='event-link' data-url='"+this.link+"' href='#'>"+this.event+"</a></li>");
         });
+
+        bind_anchor_clicks()
 
     });
 
     console.log("End filling event list");
+}
+
+function bind_anchor_clicks() {
+    $(".event-link").bind("click", function() {
+        $("#url").val($(this).data("url"));
+    }); // end click Function
 }
